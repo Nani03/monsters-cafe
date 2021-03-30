2b86a8b44b2a7d3409db28b4a766410db2b1bbf0
nani03 <leela.rroyal@gmail.com>
2021-03-30 21:54:20 +0530

    first project commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import React, { Component } from "react";
+import { CardList } from "./Components/card-list/card-list.component";
+import { SearchBox } from "./Components/search-box/search-box.component";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resoponse) => resoponse.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monster Cafe</h1>
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
